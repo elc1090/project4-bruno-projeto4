6eb2a45e88f69059de0b196d6bbf4bbed391bb40
Bruno Frizzo <brunofrizzo@outlook.com>
2023-05-19 00:01:23 -0300

    tailwind adicionado. configuracoes inicias do PWA.

---
+++ src/service-worker.js
@@ -0,0 +1,28 @@
+// Defina o nome do cache
+const cacheName = 'my-pwa-cache';
+
+// Lista de arquivos a serem armazenados em cache
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/icon.png'
+];
+
+// Instalação do Service Worker e armazenamento em cache dos arquivos
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(filesToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Interceptação das solicitações e fornecimento dos recursos em cache (offline-first)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
